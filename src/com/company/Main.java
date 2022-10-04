ae58462f9086c9fc82de82c1bf964eeccf9b0341
Andrew <zozo1901@mail.ru>
2022-10-04 13:46:49 +0300

    added logic of game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Напишите максимальное число");
+        int max = scanner.nextInt();
+        System.out.println("Напишите минимальное число");
+        int min = scanner.nextInt();
+        max = max-min;
+        int random = (int) (Math.random() * max + 1) + min;
+        int count = 0;
+        while (true) {
+            System.out.println("Введите число: ");
+            int num = scanner.nextInt();
+            count++;
+            if (num<random){
+                System.out.println("Число больше заданого, попробуйте ещё");
+            }else if(num>random){
+                System.out.println("Число меньше заданого, попробуйте ещё");
+            } else if(num == random){
+                System.out.println("Вы угадали, вы молодец, кол-во попыток: "+count);
+                break;
+            }
+        }
     }
 }
